f7de2714f8d25dcad5861f3772b6beb2e701edb7
pranjali chopade <chopadepranjali3@gmail.com>
2024-10-02 08:46:35 -0400

    navbar & message card

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import AuthProvider from "@/context/AuthProvider";
 
-import { Toaster } from "@/components/ui/toaster"
+
+
 import Navbar from "@/components/Navbar";
 
 
@@ -31,14 +31,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <AuthProvider>
+      
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
          <Navbar/>
           {children}
-          <Toaster />
+          
         </body>
-      </AuthProvider>
+    
     </html>
   );
 }
